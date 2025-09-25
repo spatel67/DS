10faeeb4c2577ec73236936c337ebd7a82d09ae1
spatel67 <shirali.p123@gmail.com>
2025-09-25 10:22:01 -0500

    hehehe

--- DS2/src/DS2_Printer_Queue.java
+++ DS2/src/DS2_Printer_Queue.java
@@ -4,6 +4,8 @@ public class DS2_Printer_Queue
 {
     public static void main(String[] args)
     {
+        MyQueue<Job> queue= new MyQueue<>();
+
         try
         {
             System.out.println("Enter job file name:");
@@ -20,11 +22,39 @@ public class DS2_Printer_Queue
                 fromFile.createNewFile();
             }
 
+            int x = 0;
+            int subTime = 0;
+            int page = 0;
+
 
             while (keyboard.hasNextLine())
             {
-                MyQueue
+                x++;
+                String[] sigma = keyboard.nextLine().split(",");
+                subTime = Integer.parseInt(sigma[0]);
+                page = Integer.parseInt(sigma[1]);
+                Job job = new Job(x, subTime, page);
+                queue.offer(job);
+
             }
+
+
+            for(int a = 0; a<=x; a++)
+            {
+                System.out.println("Time " + queue.element().getPrintStart() + "s: Job #" + a + " Recieved (" + queue.element().getPages()+ " pages)");
+                System.out.println("Time " + queue.element().getBufferingStart() + "s: Job #" + a + " Buffering Started");
+                System.out.println("Time " + queue.element().getBufferingEnd() + "s: Job #" + a + " Finished Buffering and Started Printing");
+                System.out.println("Time " + queue.poll().getPrintEnd() + "s: Job #" + a + " Finished Printing");
+
+            }
+
+            System.out.println(" ");
+            System.out.println("Printing Simulations Compete.");
+            System.out.println(" ");
+            System.out.println("Total Print Jobs: " + x);
+            System.out.println("Average Wait Time: ") ;
+
+
 
 
 
